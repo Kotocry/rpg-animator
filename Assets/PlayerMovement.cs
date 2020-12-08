d18c8377a542354c08a9e0799bedcdb0ef7943c2
Kotocry <chekalenko.daniel@mail.ru>
2020-12-08 22:48:56 +0100

    repo repo

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    public void Jump()
+    {
+        animator.SetTrigger("Jump");
+    }
+}
---
+++ Assets/Sprint.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprint : MonoBehaviour
+{
+    float moveSpeed = 5f;
+    float dirY;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
